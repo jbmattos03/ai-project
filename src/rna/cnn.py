1599da9d16a1702f106fbb3245444db65e4c3a3d
jbmattos03 <jbmattos2003@gmail.com>
2025-06-29 22:56:13 -0300

    feat(cnn): Add cnn.py

--- src/rna/cnn.py
+++ src/rna/cnn.py
@@ -1,8 +1,20 @@
-import torch
+from torch import *
+
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from logger import logger_config
+
+logger = logger_config(process_name="CNN")
 
 class CNN(torch.nn.Module):
     def __init__(self):
+        logger.info("Initializing CNN model")
         pass
 
     def forward(self, x):
         pass
+
+if __name__ == "__main__":
+    logger.info("Running CNN model as main script")
+    model = CNN()
+    logger.info("CNN model initialized successfully")
